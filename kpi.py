28965cda0ab730158056ef17601bc1622307cf8c
Fasri <felipeasr@gmail.com>
2025-07-31 06:58:32 -0300

    desafio

---
+++ kpi.py
@@ -0,0 +1,13 @@
+# Solicitar o nome do usuario
+nome = input('Qual o se nome ? : ')
+
+#Solicitar o valor do salario
+salario = float(input('Qual o valor do salario? : '))
+
+# Solicitar o valor percentual do bonus
+bonus = float(input('Qual o valor do seu bonus? : '))
+
+#Calculo do KPI
+kpi = 1000 + salario * bonus
+
+print(f'Ola {nome} o seu valor bonus foi de {kpi}')
