6a93b702bdae3929efa7c974328c117b1254fc04
Fasri <felipeasr@gmail.com>
2025-08-21 09:31:03 -0300

    execicio 2 soma

---
+++ exercicio02.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int (input("Digite o segundo numero:"))
+
+# soma
+soma = numero1 + numero2
+
+print(f" o resultado da soma e : {soma}")
